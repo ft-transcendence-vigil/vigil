e5ad2f23e504700b8a05d6539472c76d5a11a76d
ELamraniG <moha.mati@hotmail.com>
2026-09-03 21:26:57 +0100

    feat(api): add SessionRepository

--- services/api/src/main/java/com/ft_transcendence/vigil/repositories/SessionRepository.java
+++ services/api/src/main/java/com/ft_transcendence/vigil/repositories/SessionRepository.java
@@ -1,10 +1,13 @@
 package com.ft_transcendence.vigil.repositories;
 
 import com.ft_transcendence.vigil.domain.entities.Session;
+import com.ft_transcendence.vigil.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 @Component
 public interface SessionRepository extends JpaRepository<Session, UUID> {
+    List<Session> findByUserAndRevokedFalse(User user);
 }
