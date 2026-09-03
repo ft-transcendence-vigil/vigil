6ea20dcc145de370ed642a44a06e4bea1458d5e0
ELamraniG <moha.mati@hotmail.com>
2026-09-03 21:26:58 +0100

    feat(api): add login service

--- services/api/src/main/java/com/ft_transcendence/vigil/Services/AuthService.java
+++ services/api/src/main/java/com/ft_transcendence/vigil/Services/AuthService.java
@@ -1,24 +1,32 @@
 package com.ft_transcendence.vigil.Services;
-
 import com.ft_transcendence.vigil.Security.JjwtService;
+import com.ft_transcendence.vigil.domain.dtos.LoginDto;
+import com.ft_transcendence.vigil.domain.dtos.SessionDto;
 import com.ft_transcendence.vigil.domain.dtos.SetupDto;
 import com.ft_transcendence.vigil.domain.entities.RefreshToken;
 import com.ft_transcendence.vigil.domain.entities.Session;
 import com.ft_transcendence.vigil.domain.entities.User;
 import com.ft_transcendence.vigil.domain.entities.UserPrincipal;
 import com.ft_transcendence.vigil.exceptions.DuplicatedResourcesException;
+import com.ft_transcendence.vigil.exceptions.ResourcesNotFoundException;
+import com.ft_transcendence.vigil.exceptions.UnauthorizedException;
+import com.ft_transcendence.vigil.mappers.SessionMapper;
 import com.ft_transcendence.vigil.mappers.SetupMapper;
 import com.ft_transcendence.vigil.repositories.RefreshTokenRepository;
+import com.ft_transcendence.vigil.repositories.SessionRepository;
 import com.ft_transcendence.vigil.repositories.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
+
 
 @RequiredArgsConstructor
 @Service
@@ -26,15 +34,16 @@ import java.util.List;
 
 public class AuthService {
     public record AuthResult(String accessToken, String rawRefreshToken, String role) {}
-
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
     private final JjwtService jjwtService;
     private final SetupMapper setupMapper;
     private final UserRepository userRepository;
-    private final RefreshTokenRepository refrechTokenRepository;
-
+    public final RefreshTokenRepository refreshTokenRepository;
+    private final SessionRepository sessionRepository;
+    private final SessionMapper sessionMapper;
     // I create a session and refresh token and return the string of  refresh token
-    public String createSessionAndRefreshToken(User user, HttpServletRequest request)
+    private String createSessionAndRefreshToken(User user, HttpServletRequest request)
     {
         String raw = jjwtService.generateRefreshToken();
 
@@ -49,10 +58,9 @@ public class AuthService {
                 .session(session)
                 .expiresAt(Instant.now().plusMillis(jjwtService.getRefrechTokenExpiration()))
                 .build();
-        session.setRefreshTokens(List.of(refreshToken));
-        user.setSessions(List.of(session));
-        user.setRefreshTokens(List.of(refreshToken));
 
+        sessionRepository.save(session);
+        refreshTokenRepository.save(refreshToken);
         return raw;
     }
 
@@ -66,9 +74,22 @@ public class AuthService {
         User user = setupMapper.map(setupDto);
         user.setPasswordHash(passwordEncoder.encode(setupDto.getPassword()));
         user.setRole("admin");
-        String rawRefreshToken = createSessionAndRefreshToken(user, request);
         userRepository.save(user);
+        String rawRefreshToken = createSessionAndRefreshToken(user, request);
         String accessToken = jjwtService.generateToken(new UserPrincipal(user));
         return new AuthResult(accessToken, rawRefreshToken, user.getRole());
     }
+
+    @Transactional
+    public AuthResult loginService(LoginDto loginDto, HttpServletRequest request){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(),loginDto.getPassword()));
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        User user = userPrincipal.getUser();
+        String rawRefreshToken = createSessionAndRefreshToken(user, request);
+        String accessToken = jjwtService.generateToken(new UserPrincipal(user));
+        return new AuthResult(accessToken, rawRefreshToken, user.getRole());
+    }
+
+
+
 }
