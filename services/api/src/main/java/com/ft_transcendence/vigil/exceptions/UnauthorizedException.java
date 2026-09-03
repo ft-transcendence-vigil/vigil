7c76e57734f486709b6a968b307cad0aca791f84
ELamraniG <moha.mati@hotmail.com>
2026-09-03 21:26:57 +0100

    feat(api): add UnauthorizedException

---
+++ services/api/src/main/java/com/ft_transcendence/vigil/exceptions/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.ft_transcendence.vigil.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message)
+    {
+        super(message);
+    }
+}
