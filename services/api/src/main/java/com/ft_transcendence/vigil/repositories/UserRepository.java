bc2effdbf4719224fd648413487b9b1d98214c2d
ELamraniG <moha.mati@hotmail.com>
2026-09-03 21:26:57 +0100

    feat(api): add username lookup to UserRepository

--- services/api/src/main/java/com/ft_transcendence/vigil/repositories/UserRepository.java
+++ services/api/src/main/java/com/ft_transcendence/vigil/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ft_transcendence.vigil.repositories;
 
 import com.ft_transcendence.vigil.domain.entities.User;
+import com.ft_transcendence.vigil.exceptions.ResourcesNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,5 @@ import java.util.Optional;
 import java.util.UUID;
 @Component
 public interface UserRepository extends JpaRepository<User, UUID> {
-    public Optional<User> findByEmail(String email) throws UsernameNotFoundException;
+    public Optional<User> findByEmail(String email) throws ResourcesNotFoundException;
 }
