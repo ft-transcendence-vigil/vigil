546b9945d0b9ffbc642eb70679250110cd0a017d
ELamraniG <moha.mati@hotmail.com>
2026-09-24 23:32:40 +0000

    added sse registry for live/trace

--- services/api/src/main/java/com/ft_transcendence/vigil/serversentevents/TraceSseRegistry.java
+++ services/api/src/main/java/com/ft_transcendence/vigil/serversentevents/TraceSseRegistry.java
@@ -1,7 +1,9 @@
 package com.ft_transcendence.vigil.serversentevents;
 
-import com.ft_transcendence.vigil.domain.dtos.telemetry.Log;
-import com.ft_transcendence.vigil.repositories.clickhouse.LogRepository;
+
+import com.ft_transcendence.vigil.domain.dtos.telemetry.Trace;
+
+import com.ft_transcendence.vigil.repositories.clickhouse.TraceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -9,48 +11,44 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-    @Slf4j
-    @RequiredArgsConstructor
-    @Component
-    public class LogSseRegistry {
-        private final LogRepository logRepository;
-        private record SseData(String severity, String service, SseEmitter emitter) {
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TraceSseRegistry {
+    private final TraceRepository traceRepository;
+    private record SseData(String service,SseEmitter emitter) {
 
-        }
+    }
 
-        private List<SseData> sseDataList = new CopyOnWriteArrayList<>();
+    private List<SseData> sseDataList = new CopyOnWriteArrayList<>();
 
-        public SseEmitter addSseEmitter(String severity, String service) {
-            SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-            SseData sseData = new SseData(severity, service, emitter);
-            emitter.onTimeout(() -> sseDataList.remove(sseData));
-            emitter.onError((e) -> sseDataList.remove(sseData));
-            emitter.onCompletion(() -> sseDataList.remove(sseData));
-            sseDataList.add(sseData);
-            return emitter;
-        }
+    public SseEmitter addSseEmitter(String service) {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        SseData sseData = new SseData(service, emitter);
+        emitter.onTimeout(() -> sseDataList.remove(sseData));
+        emitter.onError((e) -> sseDataList.remove(sseData));
+        emitter.onCompletion(() -> sseDataList.remove(sseData));
+        sseDataList.add(sseData);
+        return emitter;
+    }
 
-        public void broadCastLog(Log ourlog) {
-            for (SseData data : sseDataList) {
-                if (data.severity == null || data.severity.equals(ourlog.getSeverity())) {
-                    if (data.service == null || data.service.equals(ourlog.getService())) {
-                        {
-                            try {
-                                data.emitter.send(ourlog);
-                            } catch (Exception e) {
-                                log.error(e.getMessage());
-                                data.emitter.completeWithError(e);
-                            }
+    public void broadCastTrace(Trace ourTrace) {
+        for (SseData data : sseDataList) {
+                if (data.service == null || data.service.equals(ourTrace.getService())) {
+                    {
+                        try {
+                            data.emitter.send(ourTrace);
+                        } catch (Exception e) {
+                            log.error(e.getMessage());
+                            data.emitter.completeWithError(e);
                         }
                     }
                 }
 
-            }
-
-
         }
 
+
     }
+
+}
